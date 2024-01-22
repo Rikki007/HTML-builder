067560a509feb1c0ca346f26fcb2a50212f62df8
Kirill Oreshkevich <Oreshaodnoglazy@gmail.com>
2024-01-22 21:14:59 +0300

    feat: add solution of 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const readFile = fs.createReadStream('./01-read-file/text.txt', 'utf-8');
+let data = ''
+readFile.on('data', (chunk) => {
+  data += chunk;
+});
+readFile.on('end', () => {
+  console.log(data);
+});
+readFile.on('error', (error) => {
+  console.log('error, may be wrong directory in terminal', error.massage)
+})
