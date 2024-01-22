83f00ef56b558033f9538af5cb1372ae7108ad4a
Kirill Oreshkevich <Oreshaodnoglazy@gmail.com>
2024-01-22 21:18:18 +0300

    fix: fix code

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const readFile = fs.createReadStream('./01-read-file/text.txt', 'utf-8');
-let data = ''
+let data = '';
 readFile.on('data', (chunk) => {
   data += chunk;
 });
@@ -8,5 +8,5 @@ readFile.on('end', () => {
   console.log(data);
 });
 readFile.on('error', (error) => {
-  console.log('error, may be wrong directory in terminal', error.massage)
-})
+  console.log('error, may be wrong directory in terminal', error.massage);
+});
