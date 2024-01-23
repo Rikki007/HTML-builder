fd301630968b9cb9d5e344aa16a299f19e8a660c
Kirill Oreshkevich <Oreshaodnoglazy@gmail.com>
2024-01-23 21:07:33 +0300

    fix: change function

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,21 +1,57 @@
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 const { stdin, stdout } = process;
-fs.writeFile(
+const rl = readline.createInterface(stdin, stdout);
+
+// greetings
+console.log('Hi');
+
+// create file or say that file is already exist
+fs.readFile(
   path.join(__dirname, '02-text.txt'),
-  '',
-  (err) => {
-    if (err) throw err;
-    console.log('file was creted, ready to record data');
+  "utf-8",
+  (err, data) => {
+    
+    if (err) {
+      fs.writeFile(
+        path.join(__dirname, '02-text.txt'),
+        '',
+        (err) => {
+          if (err) throw err;
+          console.log("File was created, ready to update file");
+        },
+      );
+    }
+    if (!err) {
+      console.log('File is exist, ready to update file');
+    }
   },
 );
-stdin.on('data', (data) => {
+
+// check enter value
+rl.on('line', (data) => {
+  if (data.toString() === 'exit') {
+    console.log('bye');
+    rl.close();
+  }
   fs.appendFile(
     path.join(__dirname, '02-text.txt'),
-    data.toString(),
+    data + '\n',
     (err) => {
       if (err) throw err;
       console.log('file was modified');
     },
   );
 });
+
+// exit if press ctrl+c
+rl.on('SIGINT', () => {
+  console.log('bye');
+  rl.close();
+})
+
+rl.on('close', () => {
+  process.exit();
+});
+
